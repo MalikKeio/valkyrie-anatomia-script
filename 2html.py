4eb82a9b316c9d436ee0ac931172b858b582b973
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-06-19 06:16:11 +0900

    Display inline subchapters
    Add google analytics

--- 2html.py
+++ 2html.py
@@ -138,6 +138,16 @@ class HTMLFile:
 <body>
     <h1>%s</h1>
     %s
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-79560349-1', 'auto');
+    ga('send', 'pageview');
+
+    </script>
 </body>
 </html>''' % (self.path, self.title, self.title, self.body)
         return html
