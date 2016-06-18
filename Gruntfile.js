6e5c64a2f630c5b280e5ed6343c5ef140e940f55
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-06-19 00:14:59 +0900

    Auto packaging and upload with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+//Gruntfile.js
+
+module.exports = function(grunt) {
+
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    // configure jshint to validate js files -----------------------------------
+    watch: {
+      scripts: {
+        files: ['**/*.vp'],
+        tasks: ['shell'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+    shell: {
+        options: {
+            stderr: false
+        },
+        target: {
+            command: './2english-init.py && ./2html.py && ./upload.sh'
+        }
+    }
+  });
+
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
+
+};
---
+++ upload.sh
@@ -0,0 +1,1 @@
+scp -r html/ malik@fj.ics.keio.ac.jp:/home/malik/wwwroot/share-stuff/valkyrie-anatomia-script/
