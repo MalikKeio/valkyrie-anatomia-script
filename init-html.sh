3cd17549602dfabe672898bab0087c705bd53943
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-07-10 21:19:57 +0900

    Update init and upload script

---
+++ init-html.sh
@@ -0,0 +1,3 @@
+cd html/
+git init
+git remote add origin git@github.com:valkyrieanatomia/valkyrieanatomia.github.io.git
--- upload.sh
+++ upload.sh
@@ -1,1 +1,4 @@
-scp -r html/ malik@fj.ics.keio.ac.jp:/home/malik/wwwroot/share-stuff/valkyrie-anatomia-script/
+cd html/
+git add .
+git commit -m "Website update"
+git push -f
